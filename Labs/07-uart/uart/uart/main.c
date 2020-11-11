859095dc5d0485320960bc6b49e90285c410c63b
libormatejek <lmatejek1@gmail.com>
2020-11-11 13:07:32 +0100

    update

---
+++ Labs/07-uart/uart/uart/main.c
@@ -0,0 +1,18 @@
+/*
+ * uart.c
+ *
+ * Created: 11.11.2020 12:20:22
+ * Author : Libor
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
